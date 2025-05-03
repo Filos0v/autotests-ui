c7f4718889f23bc12062cfb69db47e7f7a7e9c6d
Filos0v <filosovsheva@gmail.com>
2025-05-03 18:50:08 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,30 +1,27 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
-
+# Открываем браузер с использованием Playwright
 with sync_playwright() as playwright:
+    # Запускаем Chromium браузер в обычном режиме (не headless)
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
+    context = browser.new_context()
+    # Открываем новую страницу в рамках контекста
+    page = context.new_page()
 
-    # Открываем страницу регистрации
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    # Заполнение email
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill('user.name@gmail.com')
 
-    # Заполнение username
     username_input = page.get_by_test_id('registration-form-username-input').locator('input')
     username_input.fill('username')
 
-    # Заполнение password
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill("password")
+    password_input.fill('password')
 
-    # Нажатие на кнопку регистрации
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
-    # Проверка, что Dashboard есть на странице
-    dashboard = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard).to_be_visible()
-    expect(dashboard).to_have_text("Dashboard")
+    # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
+    context.storage_state(path="browser-state.json")
